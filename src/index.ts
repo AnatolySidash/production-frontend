fc1f9a0c0c40227bfca9c1f52435bbeea242fa28
Anatoly SIdash <sidash85@yandex.ru>
2024-05-10 12:54:44 +0300

    feat: project started, add basic webpack config

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { testFunction } from "./test";
+
+
+testFunction(123)
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export function testFunction(arg: number): string {
+   console.log("Function activated")
+
+   return ''
+}
