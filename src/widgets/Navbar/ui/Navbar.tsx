4537027a92b7a1d95574505bf0f306a19ef2427f
Anatoly SIdash <sidash85@yandex.ru>
2024-05-12 22:43:00 +0300

    feat: add svg/png/jpeg file loader, add button component

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,15 +1,18 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import mainClass from './Navbar.module.scss';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 
 interface NavbarProps {
    className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+
    return (
-      <div className={mainClass.links}>
-         <nav className={classNames(mainClass.Navbar, {}, [className])}>
+      <div className={classNames(mainClass.Navbar, {}, [className])}>
+         <ThemeSwitcher />
+         <nav className={mainClass.links}>
             <AppLink
                theme={AppLinkTheme.SECONDARY}
                to={'/'}>Главная
